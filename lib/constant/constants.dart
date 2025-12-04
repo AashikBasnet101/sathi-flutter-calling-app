d959a6b4e35c975db4070427d52f664ffec2467c
aashikBasnet <greatfriendsofsevend@gmail.com>
2025-12-04 19:53:22 +0545

    Update appID and appSign placeholders in constants

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,6 +1,6 @@
 class Appinfo {
   static const appName = "Sathi";
-  static const appID = "1633579635";
+  static const appID = "your appID";
   static const appSign =
-      "45cf662a5b7e141692f432393cadfa65a37583dcdd10c601be14c9496dc48d70"; 
+      "your appsign"; 
 }
